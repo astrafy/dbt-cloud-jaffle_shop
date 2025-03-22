fe97ddd5d03f6ed5bfff40437fadf9ece85d25e0
Charles Verleyen <charles@astrafy.io>
2025-03-22 16:52:06 +0100

    debug semantic layer

--- models/datamart/orders.sql
+++ models/datamart/orders.sql
@@ -20,18 +20,8 @@ order_items_summary as (
         sum(supply_cost) as order_cost,
         sum(product_price) as order_items_subtotal,
         count(order_item_id) as count_order_items,
-        sum(
-            case
-                when is_food_item then 1
-                else 0
-            end
-        ) as count_food_items,
-        sum(
-            case
-                when is_drink_item then 1
-                else 0
-            end
-        ) as count_drink_items
+        sum(is_food_item ) as count_food_items,
+        sum(is_drink_item) as count_drink_items
 
     from order_items
 
